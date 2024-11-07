6e052c3207e26e216374305199dd1c1c5ba8fb35
sofianolasco8 <a.23308051140948@cbtis128.edu.mx>
2024-11-07 17:01:27 -0600

    Add files via upload

---
+++ pr-4_nolasco-aguilar_martha_sofia_0948_3w.py
@@ -0,0 +1,8 @@
+print(" ")
+print("nolasco_aguilar-marth_sofia_0948_3W")
+print(" ")
+thislist=[99,98,97,96,95]#se crea una lista 
+mayor = max(thislist)#define el numero mayor
+menor = min(thislist)#define el numero menor
+print("la mayor ",mayor)#imprime el numero mayor
+print("la menor",menor)#imprime el numero menor 
