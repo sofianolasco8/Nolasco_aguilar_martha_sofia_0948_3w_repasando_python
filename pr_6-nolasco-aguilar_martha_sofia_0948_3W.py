d2a324b9450a80b6c4b3683b9600291b86639e6e
sofianolasco8 <a.23308051140948@cbtis128.edu.mx>
2024-11-07 17:22:51 -0600

    Add files via upload

---
+++ pr_6-nolasco-aguilar_martha_sofia_0948_3W.py
@@ -0,0 +1,5 @@
+print(" ")
+print("nolasco_aguilar-martha_sofia_0948_3W")
+print(" ")
+thislist=["sofia","nolasco","aguilar"]#se abre una lista 
+print(thislist[0:2])#se imprie solo dos valores de la lista 
