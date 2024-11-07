b60df957021131dc8cc28ccf6f73829653903cb1
sofianolasco8 <a.23308051140948@cbtis128.edu.mx>
2024-11-07 15:49:18 -0600

    Add files via upload

---
+++ pr_1_nolasco_aguilar_martha_sofia_0949_3w.py
@@ -0,0 +1,8 @@
+print(" ")
+print("nolasco_aguilar_martha_sofia_0948_3w")
+print(" ")
+edad=int(input("ingresa tu edad ")) #indica al usuario ingresar su edad 
+if edad >= 18:#indica que si el usuario es mayor de edad mostrar un mensaje de mayor d edad
+    print("eres mayor de edad ")#el mensaje que se debe mostrar 
+else:#de lo contrario 
+    print("No eres mayor de edad ")#mostrar un mensaje de que No es mayor de edad
