fa56a38172897b1dc5f0885dd2a4f8f1018a6f44
sofianolasco8 <a.23308051140948@cbtis128.edu.mx>
2024-11-07 17:13:02 -0600

    Add files via upload

---
+++ pr_5_nolasco_aguilar_martha_sofia_0948_3w.py
@@ -0,0 +1,10 @@
+print(" ")
+print("nolasco_aguilar-martha_sofia_0948_3W")
+print(" ")
+thislist = ("manzana" , " platano" , "cereza" , "durazno" , "uva ") #se abre una lista 
+print(thislist)#se imprime la lista 
+fruta = "cereza"#se declara la variable fruta 
+if fruta in thislist:#si la variable fruta esta dentro de la lista 
+    print("cereza si esta dentro de la lista ")#se imprime mensaje 
+else:#de lo contrario 
+    print("cereza no esta dentro de la lista")#se imprime mensaje de que no esta dentro
