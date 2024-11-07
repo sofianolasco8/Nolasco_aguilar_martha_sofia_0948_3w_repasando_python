f16d1ca41fa0204c7e367a0827b8229ef3a5fe5d
sofianolasco8 <a.23308051140948@cbtis128.edu.mx>
2024-11-07 16:45:07 -0600

    Add files via upload

---
+++ pr_3_nolasco_aguilar_martha_sofia_0948_3W.py
@@ -0,0 +1,17 @@
+print(" ")
+print("nolasco_aguilar-marth_sofia_0948_3W")
+print(" ")
+nom = "daniel "
+apell = "ramos"
+
+nya=input("ingresa tu nombre y tu apellido ")
+
+
+if nya  == nom + apell:
+    print("nombre y apellido correcto ")
+else:
+    print("usuario descoocido")
+if nya ==  nom + apell:
+    print("apellido correcto")
+else:
+    print("apellido incorrecto ")
