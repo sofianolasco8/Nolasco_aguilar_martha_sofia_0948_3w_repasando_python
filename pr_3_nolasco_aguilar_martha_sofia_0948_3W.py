045f956912b875f126e11815d6b951886b812555
sofianolasco8 <a.23308051140948@cbtis128.edu.mx>
2024-11-07 16:50:56 -0600

    Add files via upload

--- pr_3_nolasco_aguilar_martha_sofia_0948_3W.py
+++ pr_3_nolasco_aguilar_martha_sofia_0948_3W.py
@@ -1,17 +1,17 @@
 print(" ")
 print("nolasco_aguilar-marth_sofia_0948_3W")
 print(" ")
-nom = "daniel "
-apell = "ramos"
+nom = "daniel " #se define una variable como nombre  
+apell = "ramos"#se define una variable como apellido
 
-nya=input("ingresa tu nombre y tu apellido ")
+nya=input("ingresa tu nombre y tu apellido ") #se le indica al usuario ingresar su nombre y apellido 
 
 
-if nya  == nom + apell:
-    print("nombre y apellido correcto ")
-else:
-    print("usuario descoocido")
-if nya ==  nom + apell:
-    print("apellido correcto")
-else:
-    print("apellido incorrecto ")
+if nya  == nom + apell: #si nya es igual que nom + apell
+    print("nombre y apellido correcto ")#se muestra un mensaje de nombre y apellido correcto 
+else:#de lo contrario 
+    print("usuario descoocido")#se muestra mensaje de usuario desconociado 
+if nya ==  nom + apell: #si nya tiene apell
+    print("apellido correcto")#se muestra mensaje de apellido correcto 
+else:#de lo contrario 
+    print("apellido incorrecto ")#se muestra mensaje de apelido incorrecto
